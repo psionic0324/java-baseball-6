b523f5bb6d0e4af0db0704c6283b0532c905bc51
psionic0324 <56951117+psionic0324@users.noreply.github.com>
2023-10-24 23:37:27 +0900

    Revert "Psionic0324"

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,27 +1,7 @@
-/**
- * 제목: 숫자 야구 게임
- */
 package baseball;
 
-import camp.nextstep.edu.missionutils.Console;
-
-/**
- * Game클래스 객체 생성, 게임 종료 후 재시작 or 종료 유무 확인
- */
 public class Application {
     public static void main(String[] args) {
-        UserInterface userInterface = new UserInterface();
-
-        userInterface.startMessage();
-        while (true) {
-            Game game = new Game();
-            game.game();
-
-            String retryCheckNumber = Console.readLine();
-            /* 재시작: 1, 종료: 2 */
-            if (retryCheckNumber.equals("2")) {
-                break;
-            }
-        }
+        // TODO: 프로그램 구현
     }
 }
